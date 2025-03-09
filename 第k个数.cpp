531a34e3fb6e6973306a3900a16e96ae2340d4d7
Xiuyi.Zhao <89612767@qq.com>
2025-03-09 14:51:17 +0800

    高精度加法.cpp

--- 第k个数.cpp
+++ 第k个数.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
+/*给定一个长度为 n的整数数列，以及一个整数 k，请用快速选择算法求出数列从小到大排序后的第 k个数。
+ 输入格式
+ 第一行包含两个整数 n 和 k。
+ 
+ 第二行包含 n 个整数（所有整数均在 1∼109范围内），表示整数数列。
+ 
+ 输出格式
+ 输出一个整数，表示数列的第 k 小数。
+ 
+ 数据范围
+ 1≤n≤100000, 1≤k≤n
+ 
+ 输入样例：
+ 5 3
+ 2 4 1 5 3
+ 输出样例：
+ 3*/
+
+ #include <iostream>
+
 using namespace std;
+
 const int N = 1e6 + 10;
 int n, k;
 int q[N];
@@ -23,5 +43,6 @@ int main()
     for (int i = 0; i < n; i++) scanf("%d", &q[i]);
     quick_sort(q, 0, n - 1);
     printf("%d", q[k - 1]);
+    
     return 0;
 }
